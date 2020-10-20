844d95deb0cffd0e43cf899d51afee2fdd99c375
Marco Mazzocchi <marco.mazzocchi.pc@gmail.com>
2020-10-20 10:03:58 +0200

    moved firebaseConfig outside repo

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,16 +1,8 @@
 import * as firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/storage'
+import { firebaseConfig } from './firebaseConfig'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAg93j6g_FfxB3-8cQAPWH6x_BQAKTPHJA',
-  authDomain: 'book-reader-46f12.firebaseapp.com',
-  databaseURL: 'https://book-reader-46f12.firebaseio.com',
-  projectId: 'book-reader-46f12',
-  storageBucket: 'book-reader-46f12.appspot.com',
-  messagingSenderId: '141703634236',
-  appId: '1:141703634236:web:a60a10d037d981a8c7d239'
-}
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 const firebaseAuth = firebase.auth()
